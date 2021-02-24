f0cb285e0966fb2debb22103bf80199532ab033a
Victor Reis <victorbsreis@gmail.com>
2021-02-24 17:38:12 -0300

    att3

--- class/usuario.php
+++ class/usuario.php
@@ -41,6 +41,8 @@ class Usuario {
 
     public function loadById($id)
     {
+        $this->setIdusuario($id);   //corrige erro null no update
+
         $sql=new Sql();
         $result = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
             ":ID"=>$id
@@ -48,12 +50,28 @@ class Usuario {
 
         //se achar o ID, já faz atribuição dos params da classe
         if(count($result) > 0) {  //ou ifexists
-            $row = $result[0];
-            $this->setIdusuario($row['idusuario']);
-            $this->setDeslogin($row['deslogin']);
-            $this->setDessenha($row['dessenha']);
-            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+            $this->setData($result[0]);
         } else echo "Usuário não encontrado.<br><br>";
+    }
+
+    public function update($login, $password) {
+        //faz a alteração de usuario/senha
+        $this->setDeslogin($login);
+        $this->setDessenha($password);
+
+        $sql = new Sql();
+        $sql->query("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID;", array( //problema desconhecido Warning: PDO::query(): SQLSTATE[00000]: No error: PDO constructor was not called in C:\ATDI\Projeto\php\dao\class\Usuario.php on line 66
+            ":LOGIN"=>$this->getDeslogin(),
+            ":PASSWORD"=>$this->getDessenha(),
+            ":ID"=>$this->getIdusuario()
+        ));
+    }
+
+
+    public function __construct($login = '', $password = '')
+    {
+        $this->setDeslogin($login);
+        $this->setDessenha($password);
     }
 
     public function __toString()
@@ -62,8 +80,61 @@ class Usuario {
             "idusuario"=>$this->getIdusuario(),
             "deslogin"=>$this->getDeslogin(),
             "dessenha"=>$this->getDessenha(),
-            "dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+            "dtcadastro"=>$this->getDtcadastro()->format('d/m/Y H:i:s')
         ));
+    }
+
+    public static function getList() {  //pode ser estático por nao usar $this
+
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+    }
+
+    public static function search($login) {  
+
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+            ':SEARCH'=>"%".$login."%"
+        ));
+    }
+
+    public function login($login, $password) {
+
+        $sql=new Sql();
+        $result = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+            ":LOGIN"=>$login,
+            ":PASSWORD"=>$password
+        ));
+
+        //se achar o ID, já faz atribuição dos params da classe
+        if(count($result) > 0) {  //ou ifexists
+            $this->setData($result[0]);
+        } else {
+            
+            throw new Exception("Login e/ou senha inválidos.");
+        }
+    }
+
+
+    public function setData($data)
+    {
+        $this->setIdusuario($data['idusuario']);
+        $this->setDeslogin($data['deslogin']);
+        $this->setDessenha($data['dessenha']);
+        $this->setDtcadastro(new DateTime($data['dtcadastro']));
+    }
+    
+    public function insert() {
+
+        $sql = new Sql();
+        $result = $sql->select("EXEC sp_usuarios_insert :LOGIN, :PASSWORD;", array(
+            ':LOGIN'=>$this->getDeslogin(),
+            ':PASSWORD'=>$this->getDessenha()
+          ));
+
+        if (count($result) > 0) {
+            $this->setData($result[0]);
+        }
     }
 
 
--- index.php
+++ index.php
@@ -1,22 +1,56 @@
 <?php
 
 require_once("config.php");
-//$sql = new Sql();
+$sql = new Sql();
 
-//n necessario, já temos classe Usuario
-//$usuarios = $sql->myQuery("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES ('vbarros', '0192837465')");
+//não necessario, já temos classe Usuario
+//$usuarios = $sql->myQuery("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES ('zeta', 'senha123')");
 //$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 //echo json_encode($usuarios);
 
 /*Opções:
  Sql::myQuery($rawQuery, $params = array())
  Sql::select($rawQuery, $params = array())
- Usuario::loadById($id)
 */
+
+//Carrega um usuário 
+/*
 $root=new Usuario();
 $root->loadById(2);
-
 echo $root; //como é um OBJETO, irá invocar o __toString dentro da classe Ususario
+*/
 
+//Carrega tdoos os usuários
+/*
+$lista = Usuario::getList();
+echo json_encode($lista);
+*/
+
+//Carrega uma lista de usuários buscando-se pelo login
+//$search = Usuario::search("b");
+//echo json_encode($search);
+
+//Carrega infos de um usuário após login
+/*
+$usr=new Usuario();
+$usr->login("bar","br34");
+echo $usr; //como é um OBJETO, irá invocar o __toString dentro da classe Ususario
+*/
+
+//Insere um usuário novo
+/*
+$aluno = new Usuario('acer','pcdell');
+
+$aluno->insert();
+echo $aluno;
+*/
+
+//Atualiza infos (login/senha) //não funciona
+/*
+$usr = new Usuario();
+$usr->loadById(6);
+$usr->update("professor", "Rec@342");
+echo $usr;
+*/
 
 ?>
